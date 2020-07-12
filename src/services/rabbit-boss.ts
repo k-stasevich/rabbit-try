90c280277186d61880c1507f15623996201f3845
Kiryl Stasevich <kirylstasevich@gmail.com>
2020-07-12 18:23:44 +0300

    fix: added log when rabbitmq is connected

--- src/services/rabbit-boss.ts
+++ src/services/rabbit-boss.ts
@@ -1,10 +1,12 @@
 import { connect, Connection } from 'amqplib';
+import { logger } from '../helpers/logger';
 
 class RabbitBoss {
   connection: Connection | null = null;
 
   async connect() {
     this.connection = await connect('amqp://localhost');
+    logger.info('Rabbitmq is connected');
   }
 }
 
